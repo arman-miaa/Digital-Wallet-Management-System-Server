911094211b9c4c9dbdc35aa1f8f037e5ed4b8489
Arman <mdarman365506@gmail.com>
2025-07-30 13:32:14 +0600

    project setup

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from "express";
+
+import cors from "cors";
+// import { router } from "./app/routes";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import notFound from "./app/middlewares/notFound";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Welcome to Digital Wallet System Backend",
+  });
+});
+
+app.use(globalErrorHandler);
+
+app.use(notFound);
+
+export default app;
